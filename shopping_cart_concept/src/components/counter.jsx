848baa3f2ece89c07ec8c756b74b28e44f110cd0
Asad <asadwaseem32@gmail.com>
2022-06-13 02:09:03 +0500

    counter component completed

--- shopping_cart_concept/src/components/counter.jsx
+++ shopping_cart_concept/src/components/counter.jsx
@@ -1,41 +1,61 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    iterator: 0,
+    items: ["item1", "item2", "item3"],
+  };
 
-    state = {
-        iterator: 0,
-        items: ['item1','item2','item3']
-    };
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
 
-    styles= {
-        fontSize: 10,
-        fontWeight: "bold"
-    }
+  render() {
+    let classes = this.getBadgeClasses();
+    return (
+      <div>
+        <span className={classes}>Count: {this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <div>
+          {this.state.items.length === 0 && "Please add an item to your cart!"}
+          {this.renderTags()}
+        </div>
+      </div>
+    );
+  }
 
-    render() {
-        let classes = this.getBadgeClasses();
-        return ( <div>
-            <span className={classes}>Count: {this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.items.map((item, ))}
-            </ul>
-          </div>);
-      }
+  //   made the method to dynamically access the object with =>
+  handleIncrement = () => {
+    this.setState({ iterator: this.state.iterator + 1 });
+  };
+  renderTags() {
+    if (this.state.items.length === 0)
+      return <p>There are no items in the cart!</p>;
+    return (
+      <ul>
+        {this.state.items.map((items) => (
+          <li key={items}>{items}</li>
+        ))}
+      </ul>
+    );
+  }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.iterator === 0 ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.iterator === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    formatCount(){
-        const { iterator } = this.state;
-        return iterator === 0 ? "Zero" : iterator;
-    }
-    };
-
-
-
+  formatCount() {
+    const { iterator } = this.state;
+    return iterator === 0 ? "Zero" : iterator;
+  }
+}
 
 export default Counter;
