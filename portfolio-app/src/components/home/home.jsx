aa6e01aec08370b3ec7b4b6920c5306cee1e9c88
Asad <asadwaseem32@gmail.com>
2022-06-17 01:48:15 +0500

    added header for portfolio app

---
+++ portfolio-app/src/components/home/home.jsx
@@ -0,0 +1,15 @@
+import "./header.css";
+import React from "react";
+
+const Header = () => {
+  return <div id="home" className="container header-container">
+    <h3>
+        <p>
+        <span>'</span>m
+        </p>
+        <h2>Front Ender Web Developer</h2>
+    </h3>
+  </div>;
+};
+
+export default Header;
