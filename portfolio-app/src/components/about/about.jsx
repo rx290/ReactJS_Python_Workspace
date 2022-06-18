5b6f6282dd001177fd9b588bb1f03144d92fe0cc
Asad <asadwaseem32@gmail.com>
2022-06-19 00:42:34 +0500

    added about me and qualifications page

---
+++ portfolio-app/src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import "./about.css";
+import Qualifications from "../qualifications/qualifications";
+
+const About = () => {
+  return (
+    <div id="about" className="container about-container">
+      <Qualifications />
+    </div>
+  );
+};
+
+export default About;
---
+++ portfolio-app/src/components/qualifications/qualifications.jsx
@@ -0,0 +1,50 @@
+import "./qualifications.css"
+
+const Qualifications = () => {
+    return (
+        <div>
+            <span className="quali-text">
+                <div className="qualifications-container">
+                    <div className="qualifications">
+                        <div className="content html"></div>
+                        <h1 >html</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content css"></div>
+                        <h1 >css</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content Javascript"></div>
+                        <h1 >Javascript</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content bootstrap"></div>
+                        <h1 >bootstrap</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content react"></div>
+                        <h1 >react</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content python"></div>
+                        <h1 >python</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content flutter"></div>
+                        <h1 >flutter</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content c-sharp"></div>
+                        <h1 >c-sharp</h1>
+                    </div>
+                    <div className="qualifications">
+                        <div className="content robot-framework"></div>
+                        <h1 >robot-framework</h1>
+                    </div>
+                </div>
+            </span>
+        </div>
+    );
+}
+ 
+export default Qualifications;
