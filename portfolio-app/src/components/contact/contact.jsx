4de8936400c5834aba5c68cc93884847eebb17c2
Asad <asadwaseem32@gmail.com>
2022-06-20 19:33:26 +0500

    project completed

--- portfolio-app/src/components/contact/contact.jsx
+++ portfolio-app/src/components/contact/contact.jsx
@@ -7,18 +7,18 @@ const Contact = () => {
         <div  id="#contact" className="container contact-container">
             <h1>contact me</h1>
             <div className="contact-links">
-                <a href="www.youtube.com" className="contact youtube">
+                <a href="https://www.youtube.com/c/TTB_Official" target="_blank" rel="noreferrer" className="contact youtube">
                 <AiOutlineYoutube className="icon"/>
                 <h2>youtube <span>Creative Ambition</span></h2>
                 </a>
             
 
-                <a href="www.whatsapp.com" className="contact whatsapp">
+                <a href="https://wa.me/+923414180647" target="_blank" rel="noreferrer" className="contact whatsapp">
                 <AiOutlineWhatsApp className="icon"/>
                 <h2>whatsapp <span>+923 414 180647</span></h2>
                 </a>
             
-                <a href="instagram/digi.drago" className="contact instagram">
+                <a href="instagram/digi.drago" target="_blank" rel="noreferrer" className="contact instagram">
                 <AiOutlineInstagram className="icon"/>
                 <h2>Instagram <span>digi.drago</span></h2>
                 </a>
--- portfolio-app/src/components/footer/footer.jsx
+++ portfolio-app/src/components/footer/footer.jsx
@@ -16,22 +16,22 @@ const Footer = () => {
         </a>
       </h1>
       <div className="social-links">
-        <a href="#">
+        <a href="https://www.instagram.com/digi.drago/" target="_blank" rel="noreferrer">
           <BsInstagram className="social" />
         </a>
-        <a href="#">
+        <a href="https://www.facebook.com/aah.thuu" target="_blank" rel="noreferrer">
           <FaFacebookF className="social" />
         </a>
         <a href="#">
           <TiSocialDribbble className="social" />
         </a>
-        <a href="#">
+        <a href="https://www.youtube.com/c/TTB_Official" target="_blank" rel="noreferrer">
           <TiSocialYoutube className="social" />
         </a>
-        <a href="#">
+        <a href="https://github.com/rx290" target="_blank" rel="noreferrer">
           <TiSocialGithub className="social" />
         </a>
-        <a href="#">
+        <a href="https://www.linkedin.com/in/muhammadasadwaseem/" target="_blank" rel="noreferrer">
           <TiSocialLinkedin className="social" />
         </a>
       </div>
